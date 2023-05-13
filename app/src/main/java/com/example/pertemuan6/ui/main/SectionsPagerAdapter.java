6970dc6a7b3bdace68532d665937a1d1819f48a7
Selvie1807 <selviipramulia@gmail.com>
2023-05-13 19:16:13 +0700

    Initial commit

---
+++ app/src/main/java/com/example/pertemuan6/ui/main/SectionsPagerAdapter.java
@@ -0,0 +1,54 @@
+package com.example.pertemuan6.ui.main;
+
+import android.content.Context;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.example.pertemuan6.R;
+import com.example.pertemuan6.ui.chat.ChatFragment;
+import com.example.pertemuan6.ui.panggilan.PanggilanFragment;
+import com.example.pertemuan6.ui.status.StatusFragment;
+
+/**
+ * A [FragmentPagerAdapter] that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    @StringRes
+    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2, R.string.tab_text_3};
+    private final Context mContext;
+
+    public SectionsPagerAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        mContext = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        if (position==0){
+            return ChatFragment.newInstance("", "");
+        }
+        else if (position==1){
+            return StatusFragment.newInstance("", "");
+        }
+        else{
+            return PanggilanFragment.newInstance("", "");
+        }
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mContext.getResources().getString(TAB_TITLES[position]);
+    }
+
+    @Override
+    public int getCount() {
+        return TAB_TITLES.length;
+    }
+}
